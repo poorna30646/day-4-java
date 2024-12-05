3ad3f384167636621c8ece33a0932eadf6eff26d
poorna30646 <thotipoornachandra@gmail.com>
2024-12-05 16:21:46 +0530

    Add files via upload

---
+++ reverse.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class vote
+{
+public static void main(String args[])
+{
+    Scanner sc=new Scanner(System.in);
+    int a=sc.nextInt();
+    while(a>0)
+    {
+        System.out.println(a);
+        a-=1;
+    }
+}
+}   
