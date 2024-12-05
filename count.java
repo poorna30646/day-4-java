f1976c670599f70aa338d4fedb08b77a1fa26127
poorna30646 <thotipoornachandra@gmail.com>
2024-12-05 13:52:39 +0530

    Add files via upload

---
+++ count.java
@@ -0,0 +1,22 @@
+import java.util.*;//checking the divisiors//
+class divisor
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        for(int j=0;j<t;j++)
+        {
+            int n=sc.nextInt();
+            int c=0;
+            for(int i=1;i<=n;i++)
+            {
+                if(n%i==0)
+                {
+                    c+=1;
+                }
+            }
+            System.out.println(c);
+        }
+    }
+}
