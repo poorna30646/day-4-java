cd12624b8928056cd538c7f7f3e9a6fd517c9c0e
poorna30646 <thotipoornachandra@gmail.com>
2024-12-05 15:07:14 +0530

    Add files via upload

---
+++ pc.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class vote
+{
+public static void main(String args[])
+{
+    Scanner sc=new Scanner(System.in);
+    int n=sc.nextInt();
+    int c=0;
+    for(int i=1;i*i<=n;i++)
+    {
+        if(n%i==0)
+        {
+            c+=1;
+            if(i*i!=n)
+            {
+                c+=i;
+            }
+
+        }
+    }
+    System.out.println(c);
+}
+}
