3a2f7ccd0abc959234b1268496d04088f44db26c
poorna30646 <thotipoornachandra@gmail.com>
2024-12-05 12:04:27 +0530

    Add files via upload

---
+++ mul8.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class fire
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        for(int i=0;i<t;i++)
+        {
+            int n=sc.nextInt();
+            if((n&7)==0)
+            {
+                System.out.println("multiple of 8");
+            }
+            else
+            {
+                System.out.println("not multiple of 8");
+
+            }
+        }
+    }
+}
